a3a1929f9c96224d1a00ec41e576eafc05a6a48e
CONG CHEN <67816@supinfo.com>
2018-08-09 17:58:45 +0200

    use rabbitmq keep stocks into the redis to speed up the request

--- src/main/java/com/cc/traffic/controller/MiaoshaController.java
+++ src/main/java/com/cc/traffic/controller/MiaoshaController.java
@@ -1,7 +1,9 @@
 package com.cc.traffic.controller;
 
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.cc.traffic.domain.MiaoshaOrder;
 import com.cc.traffic.domain.MiaoshaUser;
 import com.cc.traffic.domain.OrderInfo;
+import com.cc.traffic.rabbitmq.MQSender;
+import com.cc.traffic.rabbitmq.MiaoshaMessage;
+import com.cc.traffic.redis.GoodsKey;
 import com.cc.traffic.redis.RedisService;
 import com.cc.traffic.result.CodeMsg;
 import com.cc.traffic.result.Result;
@@ -25,7 +30,7 @@ import com.cc.traffic.vo.GoodsVo;
 
 @Controller
 @RequestMapping("/miaosha")
-public class MiaoshaController {
+public class MiaoshaController  implements InitializingBean {
 	
 	private final Logger LOGGER = Logger.getLogger(MiaoshaController.class.getName());
 
@@ -43,6 +48,9 @@ public class MiaoshaController {
 	
 	@Autowired
 	MiaoshaService miaoshaService;
+
+	@Autowired
+	MQSender sender;
 	
 	/*
 	 * 5000 threads
@@ -50,7 +58,7 @@ public class MiaoshaController {
 	 * */
     @RequestMapping(value="/do_miaosha", method=RequestMethod.POST)
     @ResponseBody
-    public Result<OrderInfo> list(Model model,MiaoshaUser user,
+    public Result<Integer> list(Model model,MiaoshaUser user,
     		@RequestParam("goodsId")long goodsId) {
     	LOGGER.info("do_miaosha is called");
     	model.addAttribute("user", user);
@@ -58,7 +66,29 @@ public class MiaoshaController {
     		LOGGER.info("----user is null");
     		return Result.error(CodeMsg.SESSION_ERROR);
     	}
-    	//判断库存
+    	/*
+    	//直接通过redis来预减库存 redis是单线程的
+    	if(redisService.decr(GoodsKey.getMiaoshaGoodsStock, ""+goodsId) < 0) {
+    		return Result.error(CodeMsg.MIAO_SHA_OVER);
+    	}
+    	*/
+        /*//直接通过redis来预减库存 redis是单线程的 和上面的代码无区别*/
+        long stock = redisService.decr(GoodsKey.getMiaoshaGoodsStock, ""+goodsId);//10
+    	if(stock < 0) {
+    		return Result.error(CodeMsg.MIAO_SHA_OVER);
+    	}
+    	//判断是否已经秒杀到了
+    	MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
+    	if(order != null) {
+    		return Result.error(CodeMsg.REPEATE_MIAOSHA);
+    	}
+    	//入队
+    	MiaoshaMessage mm = new MiaoshaMessage();
+    	mm.setUser(user);
+    	mm.setGoodsId(goodsId);
+    	sender.sendMiaoshaMessage(mm);
+    	return Result.success(0);//排队中
+    	/*//判断库存
 		GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
 		int stock = goods.getStockCount();
 		if (stock <= 0) {
@@ -74,6 +104,28 @@ public class MiaoshaController {
     	//减库存 下订单 写入秒杀订单
     	OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
     	LOGGER.info("----miaosha successfully");
-    	return Result.success(orderInfo);
+    	return Result.success(orderInfo);*/
+    }
+
+	public void afterPropertiesSet() throws Exception {
+		//add goods stock into the redis after the controller is initialized
+		List<GoodsVo> goodsList = goodsService.listGoodsVo();
+		if(goodsList == null) {
+			return;
+		}
+		for(GoodsVo goods : goodsList) {
+			redisService.set(GoodsKey.getMiaoshaGoodsStock, ""+goods.getId(), goods.getStockCount());
+		}
+	}
+	@RequestMapping(value="/result", method=RequestMethod.GET)
+    @ResponseBody
+    public Result<Long> miaoshaResult(Model model,MiaoshaUser user,
+    		@RequestParam("goodsId")long goodsId) {
+    	model.addAttribute("user", user);
+    	if(user == null) {
+    		return Result.error(CodeMsg.SESSION_ERROR);
+    	}
+    	long result  =miaoshaService.getMiaoshaResult(user.getId(), goodsId);
+    	return Result.success(result);
     }
 }
--- src/main/java/com/cc/traffic/controller/SampleController.java
+++ src/main/java/com/cc/traffic/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cc.traffic.domain.User;
+import com.cc.traffic.rabbitmq.MQSender;
 import com.cc.traffic.redis.RedisService;
 import com.cc.traffic.redis.UserKey;
 import com.cc.traffic.result.Result;
@@ -19,6 +20,8 @@ public class SampleController {
 	private UserService userService;
 	@Autowired
 	private RedisService redisService;
+	@Autowired
+	MQSender sender;
 
 	@RequestMapping("/db/get")
 	@ResponseBody
@@ -47,5 +50,32 @@ public class SampleController {
 	public Result<User> redisGet() {
 		User user = redisService.get(UserKey.getById, ""+4, User.class);
 		return Result.success(user);
+	}
+	@RequestMapping("/mq/header")
+  @ResponseBody
+  public Result<String> header() {
+		sender.sendHeader("hello,imooc");
+      return Result.success("Hello，world");
+  }
+	
+	@RequestMapping("/mq/fanout")
+  @ResponseBody
+  public Result<String> fanout() {
+		sender.sendFanout("hello,imooc");
+      return Result.success("Hello，world");
+  }
+	
+	@RequestMapping("/mq/topic")
+  @ResponseBody
+  public Result<String> topic() {
+		sender.sendTopic("hello,imooc");
+      return Result.success("Hello，world");
+  }
+
+	@RequestMapping("/mq")
+	@ResponseBody
+	public Result<String> mq() {
+		sender.send("hello,imooc");
+		return Result.success("Hello，world");
 	}
 }
