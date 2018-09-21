7d1a717f3f955e41d99262ccf4e9b81a76a6c3da
CONG CHEN <67816@supinfo.com>
2018-09-21 21:43:17 +0200

    delete some spaces

--- src/main/java/com/cc/traffic/app/Application.java
+++ src/main/java/com/cc/traffic/app/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		//run 
 		SpringApplication.run(Application.class, args);
 	}
 
